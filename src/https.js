a1c3519cf41a85c490cee217b499d76aed114b25
GuiSouza95 <guilhermesouzaa2018@gmail.com>
2025-03-06 23:58:12 +0000

    backend inserido4

--- src/https.js
+++ src/https.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 export default function getPhotosData(){
     const [availablePhotos, setAvailablePhotos] = useState([])
     useEffect(() =>{
-        fetch('http://localhost:3000/photos')
+        fetch('http://portifoliofotografia-production.up.railway.app')
         .then((response) => {return response.json()})
         .then((resData) => {
             setAvailablePhotos(resData.photos)});
