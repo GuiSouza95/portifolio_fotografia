f665beff0561e6517771ea2854021565327d8375
GuiSouza95 <guilhermesouzaa2018@gmail.com>
2025-03-07 21:30:09 +0000

    Corrigino backend images6

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-app.use(express.static('images'));
+app.use('/images', express.static('images'));
 
 const SECRET_KEY = "your_secret_key";
 
