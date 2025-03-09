d66befa9dd9bff292c2f48b902572f201da77fd7
GuiSouza95 <guilhermesouzaa2018@gmail.com>
2025-03-09 19:04:58 +0000

    atualizando fotos

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,17 +1,24 @@
 import { useState } from 'react'
 import { Link } from "react-router-dom";
+import "./HomePage.css";
 
 export default function HomePage() {
 
   return (
-    <div className='position'>
-      <h1 className='text-white mb-3'>Exercícios de fotografia</h1>
+    <div className='position text-center'>
+      <div className="container">
+        <div className='row'>
+          <h1 className='text-white mb-3'>Meu tempo livre</h1>
+        </div>
 
-      <ul>
-        <Link to="/activities">
-          <li>Atividades de fotografia</li>
-        </Link>
-      </ul>
+        <div className='row'>
+          <ul className='text-center list-group'>
+            <Link to="/activities">
+              <li className='list-group-item list-group-item-primary rounded w-25 mx-auto'>Veja aqui meu álbum</li>
+            </Link>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
